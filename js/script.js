f6951f4cbf568a8de547f948c8546f7c4fce496b
avnms <avnmsarma@gmail.com>
2024-06-03 21:53:40 +1000

    Canvas setup and added CSS rules to make the canvas responsive

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
-console.log("If you see this, then everything is working fine!");
+// Wrapping the canvas element with the window's "load" event listener
+// to ensure the script waits for all the resources
+// linked in the HTML to load completely
+addEventListener("load", function () {
+  // Canvas setup
+  const canvas = document.querySelector("canvas");
+  const context = canvas.getContext("2d");
+  canvas.width = 1500;
+  canvas.height = 500;
+});
