8930b1e1e40fb30a8e279e2e3b01e6949957e004
avnms <avnmsarma@gmail.com>
2024-06-03 22:09:05 +1000

    Created placeholder classes to handle various components of the game

--- js/script.js
+++ js/script.js
@@ -7,4 +7,31 @@ addEventListener("load", function () {
   const context = canvas.getContext("2d");
   canvas.width = 1500;
   canvas.height = 500;
+
+  // Class to keep track of the specified player input
+  class InputHandler {}
+
+  // Class to handle the projectiles (lasers and all)
+  class Projectile {}
+
+  // Class to handle the falling parts and screws
+  class Particle {}
+
+  // Class to control the main character, spritesheet to animate and so on
+  class Player {}
+
+  // Class acting as a blueprint to create and handle various enemeies
+  class Enemy {}
+
+  // Class to handle multiple backgrounds to create parllax effect
+  class Layer {}
+
+  // Class to connect all the layers and animate the entire game background
+  class Background {}
+
+  // Class to display the score, timer and other required info to the player
+  class UI {}
+
+  // Class that brings everything together to create a playable game
+  class Game {}
 });
