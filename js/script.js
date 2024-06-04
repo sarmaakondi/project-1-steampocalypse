3d724fd770c605588e346b16d604efedf07433a5
avnms <avnmsarma@gmail.com>
2024-06-04 17:04:53 +1000

    Animate enemy sprite sheets

--- js/script.js
+++ js/script.js
@@ -149,18 +149,39 @@ window.addEventListener("load", function () {
       this.markedForDeletion = false;
       this.lives = 5;
       this.score = this.lives;
+      this.frameX = 0;
+      this.frameY = 0;
+      this.maxFrame = 37;
     }
 
     update() {
-      this.x += this.speedX;
+      this.x += this.speedX - this.game.speed;
       if (this.x + this.width < 0) {
         this.markedForDeletion = true;
+      }
+      // Sprite animation
+      if (this.frameX < this.maxFrame) {
+        this.frameX++;
+      } else {
+        this.frameX = 0;
       }
     }
 
     draw(context) {
-      context.fillStyle = "tomato";
-      context.fillRect(this.x, this.y, this.width, this.height);
+      if (this.game.debug) {
+        context.strokeRect(this.x, this.y, this.width, this.height);
+      }
+      context.drawImage(
+        this.image,
+        this.frameX * this.width,
+        this.frameY * this.height,
+        this.width,
+        this.height,
+        this.x,
+        this.y,
+        this.width,
+        this.height
+      );
       context.fillStyle = "black";
       context.font = "25px Roboto";
       context.fillText(this.lives, this.x, this.y);
@@ -170,9 +191,11 @@ window.addEventListener("load", function () {
   class Angler1 extends Enemy {
     constructor(game) {
       super(game);
-      this.width = 228 * 0.3;
-      this.height = 169 * 0.3;
+      this.width = 228;
+      this.height = 169;
       this.y = Math.random() * (this.game.height * 0.9 - this.height);
+      this.image = document.getElementById("angler1");
+      this.frameY = Math.floor(Math.random() * 3);
     }
   }
 
@@ -234,7 +257,7 @@ window.addEventListener("load", function () {
     constructor(game) {
       this.game = game;
       this.fontSize = 25;
-      this.fontFamily = "Roboto";
+      this.fontFamily = "League Gothic";
       this.color = "white";
     }
 
